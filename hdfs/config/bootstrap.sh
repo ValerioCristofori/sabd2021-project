d1371ea9a947a3a8610751528747b9f7c9d71241
valeriocristofori <valeriocristofori@gmail.com>
2021-05-28 15:21:52 +0200

    modificato bootstrap.sh per hdfs

--- hdfs/config/bootstrap.sh
+++ hdfs/config/bootstrap.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+sudo $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 
 service ssh start
 
-if [[ "${HDFS_MODE}" == "master" ]]; then
-	echo "Start nodo master";
-	hdfs namenode -format; # delete all content of hdfs
-	$HADOOP_HDFS_HOME/sbin/start-dfs.sh;
-	hdfs dfs -mkdir input
-	hdfs dfs -mkdir output
-	hdfs dfs -cp ../../data input/
-	hdfs dfs -chown spark:spark /output
-	echo "Nodo master configurato"
-fi
+#if [[ "${HDFS_MODE}" == "master" ]]; then
+#	echo "Start nodo master";
+#	hdfs namenode -format; # delete all content of hdfs
+#	$HADOOP_HDFS_HOME/sbin/start-dfs.sh;
+#	hdfs dfs -mkdir input
+#	hdfs dfs -mkdir output
+#	hdfs dfs -cp ../../data input/
+#	hdfs dfs -chown spark:spark /output
+#	echo "Nodo master configurato"
+#fi
 
-while true; do sleep 1000; done
-wait $!;
+hdfs namenode -format; # delete all content of hdfs
+$HADOOP_HDFS_HOME/sbin/start-dfs.sh;
+hdfs dfs -mkdir input
+hdfs dfs -mkdir output
+hdfs dfs -chown spark:spark /output
+
+
+/bin/bash
